d852e714f1dbd9b1841eee8a3d444a333d7d5f01
S <77491046+sviegasdc@users.noreply.github.com>
2022-10-12 15:25:40 -0300

    terminado o duplicar com os testes na main

--- src/Main.java
+++ src/Main.java
@@ -28,21 +28,22 @@ class PilhaFlamenguista {
     // adicionar elementos (vermelhos) antes do topov
     public void pushPilhaVermelha(int elemento) {
         if (topoVermelho + 1 != topoPreto) {
-            topoVermelho++; // indo pra próxima posição pra depois inserir
+            topoVermelho++; // indo para próxima posição para depois inserir
             this.PilhaF[topoVermelho] = elemento;
         } else {
             System.out.printf("Pilha Vermelha chegou ao topo, não é possível fazer a inserção do elemento '%d'.\n", elemento);
+            duplicaArray();
         }
     }
 
     // adicionar elementos (pretos) depois do topov
     public void pushPilhaPreta(int elemento) {
-        if (topoPreto - 1 != topoVermelho) {
-            topoPreto--; // indo para próxima posição para depois inserir
-            this.PilhaF[topoPreto] = elemento; // topo do preto é o final do array
-        } else {
+        if (topoPreto - 1 == topoVermelho) {
             System.out.printf("Pilha Preta chegou ao topo, não é possível fazer a inserção do elemento '%d'\n", elemento);
+            duplicaArray();
         }
+        topoPreto--; // indo para próxima posição para depois inserir
+        this.PilhaF[topoPreto] = elemento; // topo do preto é o final do array
     }
 
     public class PilhaVaziaExcecao extends RuntimeException {
@@ -84,52 +85,78 @@ class PilhaFlamenguista {
         saida += "]";
         return saida;
     }
-//    private void duplicaArray(){
-//        int novoArray[] = new
-//    }
+    private void duplicaArray(){
+        int novoSize = this.PilhaF.length * 2;
+        int novoArray[] = new int[novoSize];
+
+        // copiar elementos vermelhos
+        for(int i=0; i<=this.topoVermelho; i++){
+            novoArray[i] = this.PilhaF[i];
+        }
+        novoSize--;
+        // copiar elementos pretos
+        for(int i=this.PilhaF.length - 1; i>=this.topoPreto; i--){
+            novoArray[novoSize--] = this.PilhaF[i];
+        }
+        this.PilhaF = novoArray;
+        this.topoPreto = this.PilhaF.length;
+    }
 }
 class Main {
     public static void main(String[] args) {
         // tamanho = N > pilhav(3) + pilhap(5)
         PilhaFlamenguista PilhaF = new PilhaFlamenguista(10);
-
-        // inserir elementos na pilha vermelha
-        System.out.println("inserindo");
-        for(int elemento = 1; elemento <= 3; elemento++){
-            System.out.println("->" + elemento);
-            PilhaF.pushPilhaVermelha(elemento);
-        }
-        System.out.println(PilhaF);
-
-
-        // inserir elementos na pilha preta
-        System.out.println("inserindo");
-        for(int elemento = 1; elemento <= 3; elemento++){
-            System.out.println("->" + elemento);
-            PilhaF.pushPilhaPreta(elemento);
-        }
-        System.out.println(PilhaF);
-
-
-        // removendo elementos na pilha vermelha
-        System.out.println(PilhaF);
-        System.out.println("retirando:");
-        for(int elemento = 1; elemento <= 5; elemento++) {
-            System.out.print(" - "+PilhaF.popPilhaVermelha());
-            System.out.println();
-        }
-        System.out.println(PilhaF);
-
-
-        // removendo elementos na pilha preta
-        System.out.println(PilhaF);
-        System.out.println("retirando:");
-        for(int elemento = 1; elemento <= 5; elemento++) {
-            System.out.print(" - "+PilhaF.popPilhaPreta());
-            System.out.println();
-        }
-        System.out.println(PilhaF);
-
-
+//
+//        // inserir elementos na pilha vermelha
+//        System.out.println("inserindo na pilha vermelha:");
+//        for(int elemento = 1; elemento <= 4; elemento++){
+//            System.out.println("->" + elemento);
+//            PilhaF.pushPilhaVermelha(elemento);
+//        }
+//        System.out.println(PilhaF);
+//
+//
+//        // inserir elementos na pilha preta
+//        System.out.println("inserindo na pilha preta:");
+//        for(int elemento = 1; elemento <= 5; elemento++){
+//            System.out.println("->" + elemento);
+//            PilhaF.pushPilhaPreta(elemento);
+//        }
+//        System.out.println(PilhaF);
+//
+//
+//        // removendo elementos na pilha vermelha
+//        System.out.println(PilhaF);
+//        System.out.println("retirando da pilha vermelha:");
+//        for(int elemento = 1; elemento <= 5; elemento++) {
+//            System.out.print(" - "+PilhaF.popPilhaVermelha());
+//            System.out.println();
+//        }
+//        System.out.println(PilhaF);
+//
+//
+//        // removendo elementos na pilha preta
+//        System.out.println(PilhaF);
+//        System.out.println("retirando da pilha preta:");
+//        for(int elemento = 1; elemento <= 5; elemento++) {
+//            System.out.print(" - "+PilhaF.popPilhaPreta());
+//            System.out.println();
+//        }
+//        System.out.println(PilhaF);
+//
+//        // teste para duplicar array
+//        System.out.println("inserindo na pilha vermelha:");
+//        for(int elemento = 1; elemento <= 17; elemento++){
+//            System.out.println("->" + elemento);
+//            PilhaF.pushPilhaVermelha(elemento);
+//        }
+//        System.out.println(PilhaF);
+//
+//        System.out.println("inserindo na pilha preta:");
+//        for(int elemento = 1; elemento <= 9; elemento++){
+//            System.out.println("->" + elemento);
+//            PilhaF.pushPilhaPreta(elemento);
+//        }
+//        System.out.println(PilhaF);
     }
 }
